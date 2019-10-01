a49a26dd3f769f8e59cbcef3bc2f23495182b6a6
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-01 15:02:36 +0100

    pass in users details to users card

--- github-user-card/src/Components/UserCard.js
+++ github-user-card/src/Components/UserCard.js
@@ -6,10 +6,19 @@ export default class UserCard extends React.Component {
 
     }
 
+    
     render() {
+        const {avatar_url, bio, followers, following, name, login, url, email} = this.props.user;
         return (
             <div>
-                
+                <img src={avatar_url} />
+                <p>Name: {name}</p>
+                <p>Username: {login}</p>
+                <p>Bio: {bio}</p>
+                <p>Email: {email}</p>
+                <p>Followers: {followers}</p>
+                <p>Following: {following}</p>
+                <p>GitHub_Url: {url}</p>
             </div>
         );
     }
