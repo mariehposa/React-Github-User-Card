9d0e3fb7fad3c51e8739e91110c1ca6e054a1777
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-01 14:40:49 +0100

    get user data using DidMount

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const githubApi = 'https://api.github.com/users/mariehposa';
+export default class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          usersDetails: {},
+      };
+  }
+
+  componentDidMount() {
+      axios.get(githubApi)
+          .then(res => {
+              console.log(res);
+              this.setState({ usersDetails: res.data });
+          });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
-
-export default App;
