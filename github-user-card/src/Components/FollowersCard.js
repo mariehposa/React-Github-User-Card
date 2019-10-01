31ff5919ced5854c13e344fe4ee1565f76e1faa1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-01 15:32:59 +0100

    create cards for followers

--- github-user-card/src/Components/FollowersCard.js
+++ github-user-card/src/Components/FollowersCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class UserCard extends React.Component {
+export default class FollowersCard extends React.Component {
     constructor (props) {
         super(props);
 
@@ -8,18 +8,15 @@ export default class UserCard extends React.Component {
 
     
     render() {
-        const {avatar_url, bio, followers, following, name, login, url, email, organizations_url} = this.props.user;
+        const {avatar_url, bio, followers, following, name, login, url, email} = this.props.user;
         return (
             <div>
                 <img src={avatar_url} />
                 <p>Name: {name}</p>
                 <p>Username: {login}</p>
-                <p>Bio: {bio}</p>
-                <p>Email: {email}</p>
                 <p>Followers: {followers}</p>
                 <p>Following: {following}</p>
                 <p>Organization: {organizations_url}</p>
-                <p>GitHub_Url: {url}</p>
             </div>
         );
     }
