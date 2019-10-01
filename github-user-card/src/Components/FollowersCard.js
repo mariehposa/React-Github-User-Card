4ece16b80adf3268f3b3a9cf99b5bd32448bfac1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-01 22:12:21 +0100

    edit followers details

--- github-user-card/src/Components/FollowersCard.js
+++ github-user-card/src/Components/FollowersCard.js
@@ -8,14 +8,11 @@ export default class FollowersCard extends React.Component {
 
     
     render() {
-        const {avatar_url, bio, organizations_url, followers, following, name, login, url, email} = this.props.user;
+        const {avatar_url, organizations_url, login} = this.props.user;
         return (
             <div>
                 <img src={avatar_url} />
-                <p>Name: {name}</p>
                 <p>Username: {login}</p>
-                <p>Followers: {followers}</p>
-                <p>Following: {following}</p>
                 <p>Organizations: {organizations_url}</p>
             </div>
         );
